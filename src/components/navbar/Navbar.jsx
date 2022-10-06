f1661cbb81dbe760fc6c9e25a144979a1528dbaf
boy developer <daniel.onazi116@gmail.com>
2022-10-06 20:30:18 +0100

    saw a navigation error

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,33 +15,35 @@ function Navbar() {
     handleNavClose();
   }, [location]);
   return (
-    <header className={openNav ? 'header nav-open' : 'header'}>
-      <div className="nav-brand" onClick={() => navigate('/')}>
-        BubbleGoose 💎
-      </div>
+    <div className="Navigation">
+      <header className={openNav ? 'header nav-open' : 'header'}>
+        <div className="nav-brand" onClick={() => navigate('/')}>
+          BubbleGoose 💎
+        </div>
 
-      <ul className="nav-links">
-        <li className="nav-link">
-          <Link to="/turf" className="nav-item">
-            Turf
-          </Link>
-        </li>
-        <li className="nav-link margin">
-          <Link to="/gallery" className="nav-item">
-            Mirror
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link to="/myProfile" className="nav-item">
-            Profile
-          </Link>
-        </li>
-      </ul>
-      <button className="menu-btn" onClick={() => setOpenav((prev) => !prev)}>
-        <FaBars className="bar-btn" />
-        <FaTimes className="close-btn" />
-      </button>
-    </header>
+        <ul className="nav-links">
+          <li className="nav-link">
+            <Link to="/turf" className="nav-item">
+              Turf
+            </Link>
+          </li>
+          <li className="nav-link margin">
+            <Link to="/gallery" className="nav-item">
+              Mirror
+            </Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/myProfile" className="nav-item">
+              Profile
+            </Link>
+          </li>
+        </ul>
+        <button className="menu-btn" onClick={() => setOpenav((prev) => !prev)}>
+          <FaBars className="bar-btn" />
+          <FaTimes className="close-btn" />
+        </button>
+      </header>
+    </div>
   );
 }
 
